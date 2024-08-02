af2562345ec01fd2135a95cb9e78286b4206006d
Pranav <pranavchandra27@gmail.com>
2024-08-02 12:36:43 +0530

    not replace the characters from env file

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const googleApplicationCredentials = {
   type: process.env.GOOGLE_TYPE,
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: process.env.GOOGLE_AUTH_URI,
