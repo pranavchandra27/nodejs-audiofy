afb87b4086d5b0e773121ea5d9c93185d9909e2a
Pranav <pranavchandra27@gmail.com>
2024-08-02 12:43:51 +0530

    add get / route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const textToSpeech = require('@google-cloud/text-to-speech');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const textToSpeech = require("@google-cloud/text-to-speech");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -13,7 +13,7 @@ const googleApplicationCredentials = {
   type: process.env.GOOGLE_TYPE,
   project_id: process.env.GOOGLE_PROJECT_ID,
   private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.GOOGLE_CLIENT_EMAIL,
   client_id: process.env.GOOGLE_CLIENT_ID,
   auth_uri: process.env.GOOGLE_AUTH_URI,
@@ -26,15 +26,19 @@ const client = new textToSpeech.TextToSpeechClient({
   credentials: googleApplicationCredentials,
 });
 
-app.post('/synthesize', async (req, res) => {
+app.get("/", (req, res) => {
+  res.json({ app_name: "Audiofy server" });
+});
+
+app.post("/synthesize", async (req, res) => {
   const { text, voice } = req.body;
-  const voiceName = voice === 'FEMALE' ? 'en-US-Studio-F' : 'en-US-Studio-M';
+  const voiceName = voice === "FEMALE" ? "en-US-Studio-F" : "en-US-Studio-M";
 
   const request = {
     input: { text: text },
-    voice: { languageCode: 'en-US', ssmlGender: voice, name: voiceName },
+    voice: { languageCode: "en-US", ssmlGender: voice, name: voiceName },
     audioConfig: {
-      audioEncoding: 'MP3',
+      audioEncoding: "MP3",
       speakingRate: 1.0,
       pitch: 0.0,
       volumeGainDb: 0.0,
@@ -43,11 +47,11 @@ app.post('/synthesize', async (req, res) => {
 
   try {
     const [response] = await client.synthesizeSpeech(request);
-    res.set('Content-Type', 'audio/mpeg');
+    res.set("Content-Type", "audio/mpeg");
     res.send(response.audioContent);
   } catch (error) {
-    console.error('Error synthesizing speech:', error);
-    res.status(500).send('Error synthesizing speech');
+    console.error("Error synthesizing speech:", error);
+    res.status(500).send("Error synthesizing speech");
   }
 });
 
